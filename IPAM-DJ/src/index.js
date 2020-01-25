2a7064f1006f1a61e52e959fb852bb8dd2911672
J. Celuch <jakubceluch@gmail.com>
2020-01-25 13:51:15 +0100

    Errors...

--- IPAM-DJ/src/index.js
+++ IPAM-DJ/src/index.js
@@ -1,23 +1,31 @@
 import React from "react";
-import {render} from 'react-dom'
-import {Provider} from 'react-redux'
-import {applyMiddleware, createStore} from "redux";
+import {render} from 'react-dom';
+import {Provider} from 'react-redux';
+import {applyMiddleware, createStore, compose } from "redux";
 import thunk from "redux-thunk";
 import {createLogger} from "redux-logger";
 
-import rootReducer from './reducers'
-import App from "./containers/App"
+import rootReducer from './reducers';
+import App from "./containers/App";
 
 import "./styles/styles.css";
 
-const middleware = [thunk];
-if (process.env.NODE_ENV !== 'production') {
-    middleware.push(createLogger())
-}
+import { reduxFirestore, getFirestore } from 'redux-firestore'
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
+import fireBaseConfig from './config/fireBaseConfig'
+
+//const middleware = [thunk.withExtraArgument({ getFirebase, getFirestore })];
+//if (process.env.NODE_ENV !== 'production') {
+//    middleware.push(createLogger())
+//}
 
 const store = createStore(
     rootReducer,
-    applyMiddleware(...middleware)
+    compose(
+        applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
+        reactReduxFirebase(fireBaseConfig),
+        reduxFirestore(fireBaseConfig)
+    )
 );
 
 render(
