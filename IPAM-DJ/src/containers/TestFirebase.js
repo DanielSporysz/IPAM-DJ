cc3f2fd0f654f9c5e565fac2df3eb64401b5de76
J. Celuch <jakubceluch@gmail.com>
2020-01-25 17:37:34 +0100

    Get to console.log

--- IPAM-DJ/src/containers/TestFirebase.js
+++ IPAM-DJ/src/containers/TestFirebase.js
@@ -20,7 +20,7 @@ class TestFirebase  extends React.Component {
         });
     }
 
-    addUser = e => {
+    addLocations = e => {
         e.preventDefault();
         const db = firebase.firestore();
         db.settings({
@@ -36,46 +36,29 @@ class TestFirebase  extends React.Component {
         });
     };
 
-    /*componentWillMount() {
-        var config = {
-            apiKey: "AIzaSyB_fln91xB265ISN4xoOZ1U9ZngPZBOsSM",
-            authDomain: "ipam-dj.firebaseapp.com",
-            databaseURL: "https://ipam-dj.firebaseio.com",
-            projectId: "ipam-dj",
-            storageBucket: "ipam-dj.appspot.com",
-            messagingSenderId: "960350779710",
-            appId: "1:960350779710:web:52bb81b23470b9b16c146f",
-            measurementId: "G-VWWR9EBQ2V"
-        };
-
-        firebase.initializeApp(config);
-
-        firebase.database().ref('users/2').set(
-            {
-                name: 'TestingUser2',
-                pass: 'TestingUser2pass'
-            }
-        ).then(() => {
-            console.log('INSERTED !');
-        }).catch((err) => {
-            console.log(err);
+    showLocations = e => {
+        e.preventDefault();
+        const db = firebase.firestore();
+        db.settings({
+            timestampsInSnapshots: true
         });
+        const ref = db.collection('locations');
 
-        firebase.database().ref('users').once('value', (data) => {
-            console.log(data.toJSON());
+        let allLocations = ref.get().then(snapshot => {
+            snapshot.forEach(doc => {
+                console.log(doc.id, '=>', doc.data());
+            });
+        }).catch(err => {
+            console.log('Error getting documents', err);
         });
-
-        firebase.firestore().collection('locations').add({
-            about: "Tu psy nie szczekają dupami",
-            name: "Bzdziszewo"
-        });
-    }*/
+    }
 
     render() {
         return (
             <div>
                 <TopNavBar/>
-                <form onSubmit={this.addUser}>
+                <h1>Location test</h1>
+                <form onSubmit={this.addLocation}>
                     <input
                         type="text"
                         name="about"
@@ -90,8 +73,9 @@ class TestFirebase  extends React.Component {
                         onChange={this.updateInput}
                         value={this.state.name}
                     />
-                    <button type="submit">Wyślij</button>
+                    <button type="submit" onClick={this.showLocations}>Wyślij</button>
                 </form>
+
             </div>
         );
     }
