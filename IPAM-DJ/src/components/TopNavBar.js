ce7f97301c80b361c667112fadf1ff0016d99379
J. Celuch <jakubceluch@gmail.com>
2020-01-27 20:46:10 +0100

    Some visual changes

--- IPAM-DJ/src/components/TopNavBar.js
+++ IPAM-DJ/src/components/TopNavBar.js
@@ -5,18 +5,19 @@ function TopNavBar() {
     return (
         <div className="topNavBar">
             <h1 className="title">IPAM-DJ</h1>
-            <div className="links">
-                <Link to={"/"} className="topNavBarLink">Home</Link>
-                <Link to={"/device"} className="topNavBarLink">Device</Link>
-                <Link to={"/VLANList"} className="topNavBarLink">VLAN</Link>
-                <Link to={"/NATList"} className="topNavBarLink">NAT</Link>
-                <Link to={"/location"} className="topNavBarLink">Location</Link>
-                <Link to={"/rack"} className="topNavBarLink">Rack</Link>
-                <Link to={"/subnet"} className="topNavBarLink">Subnet</Link>
-                <Link to={"/name-server"} className="topNavBarLink">Nameserver</Link>
-                <Link to={"/fb"} className="topNavBarLink">Fire</Link>
+            <div>
+                <ul className="links">
+                    <Link to={"/"} className="topNavBarLink"><li>Home</li></Link>
+                    <Link to={"/device"} className="topNavBarLink"><li>Device</li></Link>
+                    <Link to={"/VLANList"} className="topNavBarLink"><li>VLAN</li></Link>
+                    <Link to={"/NATList"} className="topNavBarLink"><li>NAT</li></Link>
+                    <Link to={"/location"} className="topNavBarLink"><li>Location</li></Link>
+                    <Link to={"/rack"} className="topNavBarLink"><li>Rack</li></Link>
+                    <Link to={"/subnet"} className="topNavBarLink"><li>Subnet</li></Link>
+                    <Link to={"/name-server"} className="topNavBarLink"><li>Nameserver</li></Link>
+                    <Link to={"/fb"} className="topNavBarLink"><li>Fire</li></Link>
+                </ul>
             </div>
-            <hr/>
         </div>
     );
 }
