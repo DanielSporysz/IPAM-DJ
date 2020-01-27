d904806b0b0e48b3eb5a59df7a9f95cf88385812
Daniel <daniel_spory@o2.pl>
2020-01-27 15:02:32 +0100

    options are always displayed in the row table

--- IPAM-DJ/src/components/TableRow.js
+++ IPAM-DJ/src/components/TableRow.js
@@ -9,28 +9,12 @@ class TableRow extends Component {
         };
     }
 
-    showOptions = () => {
-        this.setState({
-                showOptions: true
-            }
-        )
-    };
-
-    hideOptions = () => {
-        this.setState({
-                showOptions: false
-            }
-        )
-    };
-
     render() {
         let children = [];
         for (const labelId in this.props.labels) {
             if (this.props.labels.hasOwnProperty(labelId)) {
                 const labelName = this.props.labels[labelId];
-                if ((labelName !== "options" || this.state.showOptions) && this.props.values[labelName] !== null) {
-                    /* Objects generally can't be children of html so stringify them */
-                    /* Unless it's a special object created with JSX like "options" */
+                if (this.props.values[labelName] !== null) {
                     if ((typeof this.props.values[labelName] === 'object' && labelName !== "options")
                         || typeof this.props.values[labelName] === 'boolean') {
                         children.push(<td key={labelId}>{this.props.values[labelName].toString()}</td>);
